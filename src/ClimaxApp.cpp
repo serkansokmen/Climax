c3f65902b08b3fd3c15a786d913b47499bf0d17d
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-10-30 17:43:13 +0200

    KissFFT added

--- src/ClimaxApp.cpp
+++ src/ClimaxApp.cpp
@@ -7,9 +7,11 @@
 #include "cinder/audio/Output.h"
 #include "cinder/audio/FftProcessor.h"
 #include "cinder/audio/PcmBuffer.h"
+#include "cinder/audio/Callback.h"
 #include "cinder/CinderMath.h"
 #include "cinder/Timer.h"
 
+#include "KissFFT.h"
 #include "CinderConfig.h"
 
 #include "Resources.h"
@@ -36,6 +38,17 @@ private:
     Vec2f       mAttractionCenter;
     Color       mParticleColor;
     Perlin      mPerlin;
+    
+	// Analyzer
+	KissRef mFft;
+    
+    // Audio generation settings
+	float	mAmplitude;
+	float	mFreqTarget;
+	float	mPhase;
+	float	mPhaseAdjust;
+	float	mMaxFreq;
+	float	mMinFreq;
     
     float   mPerlinFrequency;
     float   mMinimumBeatForce;
@@ -87,6 +100,8 @@ public:
     void toggleSoundPlaying();
     void shutdown();
     
+    void sineWave( uint64_t inSampleOffset, uint32_t ioSampleCount, ci::audio::Buffer32f * ioBuffer );
+    
     ci::audio::SourceRef		mAudioSource;
 	ci::audio::PcmBuffer32fRef	mBuffer;
 	ci::audio::TrackRef			mTrack;
@@ -133,8 +148,20 @@ void ClimaxApp::setup()
     glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
     gl::color( ColorAf::white() );
     
-    // Load and play audio
-	mAudioSource = audio::load( getAssetPath( "vordhosbn.mp3" ).c_str() );
+    
+    // Set synth properties
+	mAmplitude = 0.5f;
+	mMaxFreq = 20000.0f;
+	mMinFreq = 1.0f;
+	mFreqTarget = 0.0f;
+	mPhase = 0.0f;
+	mPhaseAdjust = 0.0f;
+    
+//	mAudioSource = audio::load( getAssetPath( "vordhosbn.mp3" ).c_str() );
+    mAudioSource = audio::createCallback( this, & ClimaxApp::sineWave );
+    // Play sine
+//	audio::Output::play( mAudioSource );
+    
 	mTrack = audio::Output::addTrack( mAudioSource, false );
 	mTrack->enablePcmBuffering( true );
     mTrack->setLooping( true );
@@ -341,6 +368,15 @@ void ClimaxApp::mouseDown( MouseEvent event )
 void ClimaxApp::mouseMove( MouseEvent event )
 {
     mAttractionCenter = event.getPos();
+    
+    // Change frequency and amplitude based on mouse position
+	// Scale everything logarithmically to get a better feel and sound
+	mAmplitude = 1.0f - event.getY() / (float)getWindowHeight();
+	double width = (double)getWindowWidth();
+	double x = width - (double)event.getX();
+	float mPosition = (float)( ( log( width ) - log( x ) ) / log( width ) );
+	mFreqTarget = math<float>::clamp( mMaxFreq * mPosition, mMinFreq, mMaxFreq );
+	mAmplitude = math<float>::clamp( mAmplitude * ( 1.0f - mPosition ), 0.05f, 1.0f );
 }
 
 void ClimaxApp::mouseUp( MouseEvent event )
@@ -399,6 +435,45 @@ void ClimaxApp::draw()
         gl::drawSolidCircle( mAttractionCenter , mAttrFactor * 10 );
     }
     
+    // Check init flag
+	if ( mFft ) {
+        
+		// Get data in the frequency (transformed) and time domains
+		float * freqData = mFft->getAmplitude();
+		float * timeData = mFft->getData();
+		int32_t dataSize = mFft->getBinSize();
+        
+		// Cast data size to float
+		float dataSizef = (float)dataSize;
+        
+		// Get dimensions
+		float scale = ( (float)getWindowWidth() - 20.0f ) / dataSizef;
+		float windowHeight = (float)getWindowHeight();
+        
+		// Use polylines to depict time and frequency domains
+		PolyLine<Vec2f> freqLine;
+		PolyLine<Vec2f> timeLine;
+        
+		// Iterate through data
+		for ( int32_t i = 0; i < dataSize; i++ ) {
+            
+			// Do logarithmic plotting for frequency domain
+			float logSize = math<float>::log( dataSizef );
+			float x = (float)( (math<float>::log( (float)i) / logSize ) * dataSizef );
+			float y = math<float>::clamp( freqData[i] * ( x / dataSizef ) * ( math<float>::log( ( dataSizef - (float)i ) ) ), 0.0f, 2.0f );
+            
+			// Plot points on lines for tme domain
+			freqLine.push_back( Vec2f(        x * scale + 10.0f,          -y   * ( windowHeight - 20.0f ) * 0.25f + ( windowHeight - 10.0f ) ) );
+			timeLine.push_back( Vec2f( (float)i * scale + 10.0f, timeData[ i ] * ( windowHeight - 20.0f ) * 0.25f + ( windowHeight * 0.25f + 10.0f ) ) );
+            
+		}
+        
+		// Draw signals
+		gl::draw( freqLine );
+		gl::draw( timeLine );
+        
+	}
+    
     if ( mParams.isVisible() ) mParams.draw();
 }
 
@@ -414,11 +489,32 @@ void ClimaxApp::loadConfig()
     console() << "Loaded configuration from: " << getAppPath() / fs::path( mConfigFileName ) << std::endl;
 }
 
+void ClimaxApp::sineWave( uint64_t inSampleOffset, uint32_t ioSampleCount, ci::audio::Buffer32f * ioBuffer )
+{
+    // Fill buffer with sine wave
+	mPhaseAdjust = mPhaseAdjust * 0.95f + ( mFreqTarget / 44100.0f ) * 0.05f;
+	for ( uint32_t i = 0; i < ioSampleCount; i++ ) {
+		mPhase += mPhaseAdjust;
+		mPhase = mPhase - math<float>::floor( mPhase );
+		float val = math<float>::sin( mPhase * 2.0f * (float)M_PI ) * mAmplitude;
+		ioBuffer->mData[ i * ioBuffer->mNumberChannels ] = val;
+		ioBuffer->mData[ i * ioBuffer->mNumberChannels + 1 ] = val;
+	}
+    
+	// Initialize analyzer
+	if ( !mFft ) {
+		mFft = Kiss::create( ioSampleCount );
+	}
+    
+	// Analyze data
+	mFft->setData( ioBuffer->mData );
+}
+
 void ClimaxApp::shutdown()
 {
-    // Stop track
-	mTrack->enablePcmBuffering( false );
-	mTrack->stop();
+    if ( mFft ) {
+		mFft->stop();
+	}
 }
 
 CINDER_APP_NATIVE( ClimaxApp, RendererGl )
