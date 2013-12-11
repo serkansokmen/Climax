df8a878668ccfe9360c7a4b08f694e30fe6ec254
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-12-11 23:52:00 +0200

    added ParticleCluster

---
+++ src/ParticleCluster.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Particle.h"
+
+class ParticleCluster {
+    
+public:
+    
+    void addParticle( Particle * particle );
+    void removeParticle( Particle * particle );
+    
+    ci::Color color;
+    std::vector<Particle *> particles;
+};
